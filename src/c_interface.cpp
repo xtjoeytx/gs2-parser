727d0bcab518dbd34ab3d0991ff377d052258d47
死体 <xx.shitai.xx@gmail.com>
2023-06-09 00:21:14 +0200

    Add C-interface

---
+++ src/c_interface.cpp
@@ -0,0 +1,52 @@
+#include "GS2Context.h"
+
+extern "C" {
+
+	struct Response
+	{
+		bool success;
+		const char* errmsg;
+		unsigned char* bytecode;
+		uint bytecodesize;
+	};
+
+	void* get_context() {
+		return new GS2Context();
+	}
+
+	Response compile_code(void* context, const char* code, const char* type, const char* name) {
+		Response result{};
+		result.success = false;
+		auto gs2Context = (GS2Context*)context;
+
+		if (gs2Context != nullptr) {
+			std::string script = code;
+			std::string errMsg;
+			auto response = gs2Context->compile(script, type, name, true);
+
+			if (!response.errors.empty()) {
+				errMsg.clear();
+				for (const auto &err : response.errors)
+					errMsg.append(err.msg()).append("\n");
+
+				int lenStr = errMsg.length() + 1;
+				result.errmsg = new char[lenStr];
+				strcpy(const_cast<char*>(result.errmsg), errMsg.c_str());
+				result.bytecode = nullptr;
+				result.bytecodesize = 0;
+			} else {
+				result.errmsg = nullptr;
+				result.bytecode = new unsigned char[response.bytecode.size()];  // Allocate memory
+				memcpy((void*)result.bytecode, response.bytecode.buffer(), response.bytecode.size());
+				result.bytecodesize = response.bytecode.size();
+			}
+			result.success = response.success;
+		}
+
+		return result;
+	}
+
+	void delete_context(void* context) {
+		delete (GS2Context*)context;
+	}
+}
