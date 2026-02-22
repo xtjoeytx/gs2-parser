1638c38354c928aaf9d7d3469839999212fa222d
死体 <50087831+xx-shitai-xx@users.noreply.github.com>
2026-02-22 17:45:09 +0100

    Fix c interface and bindings (#34)
    * Fix C-interface and add UnitTests for dotnet bindings to catch these errors in the future
    
    * Add test step for dotnet bindings
    
    * Disable codecov for now

--- bindings/dotnet/GS2Compiler.cs
+++ bindings/dotnet/GS2Compiler.cs
@@ -1,38 +1,40 @@
 using System.Runtime.InteropServices;
 
-namespace Preagonal.Scripting.GS2Compiler
+namespace Preagonal.Scripting.GS2Compiler;
+
+public static class Interface
 {
-	public static class Interface
+	[DllImport("gs2compiler", CallingConvention = CallingConvention.Cdecl)]
+	private static extern IntPtr get_context();
+
+	[DllImport("gs2compiler", CallingConvention = CallingConvention.Cdecl)]
+	private static extern Response compile_code(IntPtr context, string? code, string? type, string? name);
+
+	[DllImport("gs2compiler", CallingConvention = CallingConvention.Cdecl)]
+	private static extern Response compile_code_no_header(IntPtr context, string? code);
+
+	[DllImport("gs2compiler", CallingConvention = CallingConvention.Cdecl)]
+	private static extern void delete_context(IntPtr context);
+
+	public static CompilerResponse CompileCode(string? code, string? type = "weapon", string? name = "npc", bool withHeader = true)
 	{
-		[DllImport("gs2compiler", CallingConvention = CallingConvention.Cdecl)]
-		private static extern IntPtr get_context();
+		var context  = get_context();
+		var response = withHeader ? compile_code(context, code, type, name) : compile_code_no_header(context, code);
 
-		[DllImport("gs2compiler", CallingConvention = CallingConvention.Cdecl)]
-		private static extern Response compile_code(IntPtr context, string? code, string? type, string? name);
-
-		[DllImport("gs2compiler", CallingConvention = CallingConvention.Cdecl)]
-		private static extern void delete_context(IntPtr context);
-
-		public static CompilerResponse CompileCode(string? code, string? type = "weapon", string? name = "npc")
+		CompilerResponse compilerResponse = new()
 		{
-			IntPtr context = get_context();
-			Response response = compile_code(context, code, type, name);
+			Success = response.Success,
+			ErrMsg  = response.ErrMsg,
+		};
 
-			CompilerResponse compilerResponse = new()
-			{
-				Success = response.Success,
-				ErrMsg = response.ErrMsg,
-			};
-
-			if (response.ByteCodeSize > 0)
-			{
-				compilerResponse.ByteCode = new byte[response.ByteCodeSize];
-				Marshal.Copy(response.ByteCode, compilerResponse.ByteCode, 0, (int)response.ByteCodeSize);
-			}
-
-			delete_context(context);
-
-			return compilerResponse;
+		if (response.ByteCodeSize > 0)
+		{
+			compilerResponse.ByteCode = new byte[response.ByteCodeSize];
+			Marshal.Copy(response.ByteCode, compilerResponse.ByteCode, 0, (int)response.ByteCodeSize);
 		}
+
+		delete_context(context);
+
+		return compilerResponse;
 	}
 }
