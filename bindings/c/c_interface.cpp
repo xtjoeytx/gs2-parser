b2aa34938b789040b83577ea26ea37ff8c1e5c1a
死体 <xx.shitai.xx@gmail.com>
2026-02-22 15:41:13 +0100

    Fix C-interface and add UnitTests for dotnet bindings to catch these errors in the future

--- bindings/c/c_interface.cpp
+++ bindings/c/c_interface.cpp
@@ -1,92 +1,87 @@
 #include "compiler/GS2Context.h"
 
 #ifdef _WIN32
-#define DLL_EXPORT __declspec(dllexport)
+  #define DLL_EXPORT extern "C" __declspec(dllexport)
 #else
-#define DLL_EXPORT
+  #define DLL_EXPORT extern "C" __attribute__((visibility("default")))
 #endif
 
-extern "C" {
-        struct Response {
-                bool Success;
-                const char *ErrMsg;
-                unsigned char *ByteCode;
-                uint32_t ByteCodeSize;
-        };
+struct Response {
+    bool Success;
+    const char *ErrMsg;
+    unsigned char *ByteCode;
+    uint32_t ByteCodeSize;
+};
 
-        DLL_EXPORT void *get_context() {
-                return new GS2Context();
+DLL_EXPORT void *get_context() {
+    return new GS2Context();
+}
+
+DLL_EXPORT Response compile_code_no_header(void *context, const char *code) {
+    Response result{};
+    result.Success = false;
+
+    if (const auto gs2Context = static_cast<GS2Context *>(context); gs2Context != nullptr) {
+		const std::string script = code;
+        std::string errMsg;
+        const auto response = gs2Context->compile(script);
+
+        if (!response.errors.empty()) {
+            errMsg.clear();
+            for (const auto &err: response.errors)
+                    errMsg.append(err.msg()).append("\n");
+
+            const int lenStr = errMsg.length() + 1;
+            result.ErrMsg = new char[lenStr];
+            strcpy(const_cast<char *>(result.ErrMsg), errMsg.c_str());
+
+            result.ByteCode = nullptr;
+            result.ByteCodeSize = 0;
+        } else {
+            result.ErrMsg = nullptr;
+            result.ByteCode = new unsigned char[response.bytecode.length()];  // Allocate memory
+            memcpy(result.ByteCode, response.bytecode.buffer(), response.bytecode.length());
+            result.ByteCodeSize = response.bytecode.length();
         }
+        result.Success = response.success;
+    }
 
-        DLL_EXPORT Response compile_code_no_header(void *context, const char *code) {
-                Response result{};
-                result.Success = false;
+    return result;
+}
 
-                auto gs2Context = (GS2Context *) context;
+DLL_EXPORT Response compile_code(void *context, const char *code, const char *type, const char *name) {
+    Response result{};
+    result.Success = false;
 
-                if (gs2Context != nullptr) {
-                        std::string script = code;
-                        std::string errMsg;
-                        auto response = gs2Context->compile(script);
+    if (const auto gs2Context = static_cast<GS2Context *>(context);
+        gs2Context != nullptr) {
+		const std::string script = code;
+        std::string errMsg;
+        const auto response = gs2Context->compile(script, type, name, true);
 
-                        if (!response.errors.empty()) {
-                                errMsg.clear();
-                                for (const auto &err: response.errors)
-                                        errMsg.append(err.msg()).append("\n");
+        if (!response.errors.empty()) {
+            errMsg.clear();
+            for (const auto &err: response.errors)
+                errMsg.append(err.msg()).append("\n");
 
-                                int lenStr = errMsg.length() + 1;
-                                result.ErrMsg = new char[lenStr];
-                                strcpy(const_cast<char *>(result.ErrMsg), errMsg.c_str());
+            const int lenStr = errMsg.length() + 1;
+            result.ErrMsg = new char[lenStr];
+            strcpy(const_cast<char *>(result.ErrMsg), errMsg.c_str());
 
-                                result.ByteCode = nullptr;
-                                result.ByteCodeSize = 0;
-                        } else {
-                                result.ErrMsg = nullptr;
-                                result.ByteCode = new unsigned char[response.bytecode.length()];  // Allocate memory
-                                memcpy((void *) result.ByteCode, response.bytecode.buffer(), response.bytecode.length());
-                                result.ByteCodeSize = response.bytecode.length();
-                        }
-                        result.Success = response.success;
-                }
-
-                return result;
+            result.ByteCode = nullptr;
+            result.ByteCodeSize = 0;
+        } else {
+            result.ErrMsg = nullptr;
+            result.ByteCode = new unsigned char[response.bytecode.length()];  // Allocate memory
+            memcpy(result.ByteCode, response.bytecode.buffer(), response.bytecode.length());
+            result.ByteCodeSize = response.bytecode.length();
         }
+        result.Success = response.success;
+    }
 
-        DLL_EXPORT Response compile_code(void *context, const char *code, const char *type, const char *name) {
-                Response result{};
-                result.Success = false;
+    return result;
+}
 
-                auto gs2Context = (GS2Context *) context;
-
-                if (gs2Context != nullptr) {
-                        std::string script = code;
-                        std::string errMsg;
-                        auto response = gs2Context->compile(script, type, name, true);
-
-                        if (!response.errors.empty()) {
-                                errMsg.clear();
-                                for (const auto &err: response.errors)
-                                        errMsg.append(err.msg()).append("\n");
-
-                                int lenStr = errMsg.length() + 1;
-                                result.ErrMsg = new char[lenStr];
-                                strcpy(const_cast<char *>(result.ErrMsg), errMsg.c_str());
-
-                                result.ByteCode = nullptr;
-                                result.ByteCodeSize = 0;
-                        } else {
-                                result.ErrMsg = nullptr;
-                                result.ByteCode = new unsigned char[response.bytecode.length()];  // Allocate memory
-                                memcpy((void *) result.ByteCode, response.bytecode.buffer(), response.bytecode.length());
-                                result.ByteCodeSize = response.bytecode.length();
-                        }
-                        result.Success = response.success;
-                }
-
-                return result;
-        }
-
-        DLL_EXPORT void delete_context(void *context) {
-                delete (GS2Context *) context;
-        }
+DLL_EXPORT void delete_context(void *context) {
+    delete static_cast<GS2Context *>(context);
 }
---
+++ bindings/dotnet/Preagonal.Scripting.GS2Compiler.UnitTests/InterfacesTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Preagonal.Scripting.GS2Compiler.UnitTests;
+
+public class InterfacesTests(ITestOutputHelper testOutputHelper)
+{
+	[Fact]
+	public void Given_script_that_is_faulty_When_compiling_Then_success_is_false_and_error_message_is_returned()
+	{
+		//Arrange
+		const string scriptText =
+			"""
+						//#CLIENTSIDE
+						function onCreated() 
+						}
+			""";
+
+
+		//Act
+		var result = Interface.CompileCode(scriptText);
+
+		//Assert
+		Assert.False(result.Success);
+		Assert.Equal("malformed input at line 3: \t\t\t}\n", result.ErrMsg);
+	}
+	
+	[Fact]
+	public void Given_script_that_is_correct_When_compiling_Then_success_is_true_and_bytecode_is_not_empty()
+	{
+		//Arrange
+		const string scriptText =
+			"""
+						//#CLIENTSIDE
+						function onCreated() {
+						}
+			""";
+
+
+		//Act
+		var result = Interface.CompileCode(scriptText);
+
+		//Assert
+		Assert.True(result.Success);
+		Assert.NotEmpty(result.ByteCode);
+	}
+	
+	[Fact]
+	public void Given_script_that_is_correct_When_compiling_without_header_Then_success_is_true_and_bytecode_is_not_empty()
+	{
+		//Arrange
+		const string scriptText =
+			"""
+						//#CLIENTSIDE
+						function onCreated() {
+						}
+			""";
+
+
+		//Act
+		var result = Interface.CompileCode(scriptText, withHeader: false);
+
+		//Assert
+		Assert.True(result.Success);
+		Assert.NotEmpty(result.ByteCode);
+	}
+}
